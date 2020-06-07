d62aa6d8af2e6507f3f8b33d9083666572ae2887
anacarinina <leilaelkhrof@gmail.com>
2020-06-07 12:53:21 +0200

    commit by zahra

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -8,34 +8,8 @@ import {MenuItem} from 'primeng/api';
 })
 export class HeaderComponent implements OnInit {
 
-  @Output() toggleSideBarForMe: EventEmitter<any> = new EventEmitter();
-
-  menuItems: MenuItem[];
-
   constructor() { }
-  ngOnInit() {    this.menuItems = [
-    {label: 'Salles', icon: 'users-class', routerLink: ['/salles']},
-    { label: 'Département', icon: 'fa-home', routerLink: ['/departements']},
-    { label: 'Responabilite', icon: 'fa-home', routerLink: ['/responsabilites']},
-    { label: 'Professeur', icon: 'fa-home', routerLink: ['/professeurs']},
-    { label: 'Surveilant', icon: 'fa-home', routerLink: ['/surveillants']},
-    { label: 'Personnel', icon: 'fa-home', routerLink: ['/personels']},
-    { label: 'Exam', icon: 'fa-home', routerLink: ['/exams']},
-    { label: 'Module', icon: 'fa-home', routerLink: ['/modules']},
-    { label: 'Etat', icon: 'fa-home', routerLink: ['/etat']},
-    { label: 'Filliere', icon: 'fa-home', routerLink: ['/fillieres']},
-    { label: 'Calendar', icon: 'fa-home', routerLink: ['/calendars']}
+  ngOnInit() {  }
 
-
-  ]; }
-
-  toggleSideBar() {
-    this.toggleSideBarForMe.emit();
-    setTimeout(() => {
-      window.dispatchEvent(
-        new Event('resize')
-      );
-    }, 300);
-  }
 
 }
